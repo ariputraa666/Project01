a485999c73fc76546d70ef13e17ce8724160d92b
ari bhawanaputra <117577163+ariputraa666@users.noreply.github.com>
2024-04-06 00:19:54 +0800

    Add files via upload

---
+++ LuasSegitiga.java
@@ -0,0 +1,26 @@
+package com.mycompany.luassegitiga;
+
+/**
+ *
+ * @author ariputraaa
+ */
+import java.util.Scanner;
+
+public class LuasSegitiga {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Meminta input alas dan tinggi dari user
+        System.out.print("Masukkan nilai alas segitiga: ");
+        double alas = scanner.nextDouble();
+        System.out.print("Masukkan nilai tinggi segitiga: ");
+        double tinggi = scanner.nextDouble();
+
+        // Menghitung luas segitiga
+        double luas = 0.5 * alas * tinggi;
+
+        // Menampilkan hasil luas segitiga
+        System.out.println("Luas segitiga dengan alas " + alas + " dan tinggi " + tinggi + " adalah " + luas);
+    }
+}
